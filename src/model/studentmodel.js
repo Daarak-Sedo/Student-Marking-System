bc0b1b650ae138d50e06b96117c1bbb78f9db49e
Bhuwan Singh Samant <107810689+Daarak-Sedo@users.noreply.github.com>
2022-12-15 14:31:02 +0530

    Add files via upload

---
+++ src/model/studentmodel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const stdSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    subject:{ type: String, required: true },
+    marks:{type:Number,required:true},
+    rollno:{type:Number,required:true,unique:true},
+    isDeleted: {
+      type: Boolean,
+      default: false
+  },
+    // email: { type: String, required: true, unique: true },
+    // password: { type: String, required: true }, 
+  
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("students", stdSchema);
